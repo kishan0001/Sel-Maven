3540137c5cc3f6e2f2949fae324c4bf7e031ceaf
Kishan <Kishan@Dell>
2019-01-05 23:36:00 +0530

    First commit

---
+++ com.sel.mav/src/test/java/com/sel/mav/First.java
@@ -0,0 +1,48 @@
+package com.sel.mav;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class First {
+	
+	WebDriver driver;
+	@BeforeClass
+	public void before(){
+		
+		System.setProperty("webdriver.chrome.driver","F:\\Study\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+	@Test
+	public void asert(){
+		
+		//default timeouts for wait is 250ms
+		
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		driver.findElement(By.name("q")).sendKeys("kishan");
+		
+		driver.findElement(By.xpath("//input[@name='btnK']")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver,6);
+		WebElement ele = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+		System.out.println(ele.getAttribute("value"));
+	    String title = driver.getTitle();
+	    System.out.println(title);
+	}
+	@AfterClass
+	public void after(){
+		
+		driver.quit();
+	}
+
+}
